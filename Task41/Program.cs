0e202833a8433af29c9ecd34fe8ca0cad1691b21
Tata <tvmezhankova@rambler.ru>
2022-12-13 17:51:33 +0300

    first commit

---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры М чисел.Посчитать, сколько чисел больше 0 ввел пользователь.
+
+
+Console.Write("Введите количество чисел массива :");
+int M = int.Parse(Console.ReadLine());
+int[] array = new int[M];
+int count = 0;
+for (int i=0; i< array.Length; i++)
+{
+  Console.Write($"Введите число{i+1}: ");
+  array[i]=int.Parse(Console.ReadLine());
+  if (array[i] >0) count++;
+}
+Console.Write($"Количество чисел больше 0 ={count}");
---
+++ Task43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа находит точку пересечения двух прямых,
+//заданных уравнениями Y=K1*X +B1   b Y=K2*X +B2.
+// Значения К1,В1,К,В2 задаются пользователем.
+
+Console.WriteLine(" Введите коэффициент К1 :");
+double K1 = double.Parse(Console.ReadLine());
+Console.WriteLine(" Введите коэффициент К2 :");
+double K2 = double.Parse(Console.ReadLine());
+Console.WriteLine(" Введите коэффициент B1 :");
+double B1 = double.Parse(Console.ReadLine());
+Console.WriteLine(" Введите коэффициент B2 :");
+double B2 = double.Parse(Console.ReadLine());
+
+double X= (B2-B1)/(K1-K2);
+double Y= K1*X + B1;
+
+Console.WriteLine($" Координаты точки пересечения ({X} , {Y})");
